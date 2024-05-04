2ff05361ccadd5200dff661445ed7647f88896dd
matthew1v <165177249+matthew1v@users.noreply.github.com>
2024-05-04 18:14:55 +0200

    Create NewCtrlClickTp.lua

---
+++ NewCtrlClickTp.lua
@@ -0,0 +1,12 @@
+local lplr = game.Players.LocalPlayer
+local tp(cf)
+    if lplr and lplr.Character then
+        lplr.Character:PivotTo(cf)
+    end
+end
+
+game.UserInputService.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 and game.UserInputService:IsKeyDown(Enum.KeyCode.LeftControl) then
+        tp(game.Players.LocalPlayer:GetMouse().Hit)
+    end
+end)
